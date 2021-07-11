3d7887c1a6bc055d6ef978c0ec63277db7ab0d29
El Roble <san_lucas2005@yahoo.com>
2021-07-10 20:23:47 -0600

    Add GET method for rendering exercise.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -55,6 +56,40 @@ app.get("/api/workouts", (req, res) => {
       res.json(err);
     });
 });
+
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+// app.get("/exercise", (req, res) => {
+
+// // db.Workout.findById(req.query.id)
+//   // .populate("exercises")
+//   //   .then(dbWorkout => {
+//   //     res.json(dbWorkout);
+//   //   })
+//   //   .catch(err => {
+//   //     res.json(err);
+//   //   });
+
+  
+// });
+
+
+app.put("/api/workouts/:id", ({body},req, res) => {
+  db.Exercise.create(body)
+    .then(({_id}) => db.Workout.findOneAndUpdate(req.params.id, { $push: { exercise: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+
 
 // app.get("/populated", (req, res) => {
 //   db.Library.find({})
